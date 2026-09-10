da622cc8daf39957d43d9aa576b4f1992265da88
Vitorinha-developeer <maria.liberal@academico.ifpb.edu.br>
2026-09-10 07:47:34 -0300

    Atualizações do controller

--- ifgram/src/main/java/ifgram/Controller/Users.java
+++ ifgram/src/main/java/ifgram/Controller/Users.java
@@ -1,8 +1,6 @@
 package ifgram.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("users")
@@ -13,6 +11,27 @@ public class Users {
 
     public String getUser(){
 
-        return "get user was called";
+        return "Olá! Chamei o get";
+    }
+
+    @PostMapping
+
+    public String postUser(){
+
+        return "Chamei o endpoint com o post";
+    }
+
+    @DeleteMapping
+
+    public String deleteUser(){
+
+        return "Chamei o delete";
+    }
+
+    @PutMapping
+
+    public String putUser (){
+
+        return "Chamei o put";
     }
 }
